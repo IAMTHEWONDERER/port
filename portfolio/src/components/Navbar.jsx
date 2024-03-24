a83760ca2734ad71a6b4b2aafe564868f16e9994
IAMTHEWONDERER <theoblivionitself@gmail.com>
2024-03-24 13:59:57 +0000

    " added an S to feedbacks, almost caused me to have a mental breakdown but we ballin "

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {Styles} from '../styles';
+import {navlinks} from '../constants';
 const Navbar = () => {
   return (
     <div>Navbar</div>
